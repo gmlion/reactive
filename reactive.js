024261c4243551afebe3335182c97e3fa04deb44
gmlion <gm_l@email.it>
2015-07-14 12:35:27 +0200

    Inibition
    Class :not-reactive added for <li>, inibiton if CTRL, ALT or other key
    is pressed

--- reactive.js
+++ reactive.js
@@ -1,11 +1,18 @@
 jQuery(document).ready( function (){
 	
-	jQuery('.menu a').click( function() {
-		jQuery(this).css( 'font-size', '90%');
-		var href = this.href;
-		jQuery('#primary').css( "visibility", "hidden" );
-		location.href = href;
-		return false;
+	jQuery('.menu li:not(.not-reactive) a').click( function(event) {
+        if (!
+            (event.ctrlKey ||
+            event.shiftKey ||
+            event.metaKey ||
+            (event.button && event.button == 1))) {
+            event.preventDefault();
+            jQuery(this).css( 'font-size', '90%');
+            var href = this.href;
+            jQuery('#primary').css( "visibility", "hidden" );
+            location.href = href;
+        }
+		
 	});
 } );
 
